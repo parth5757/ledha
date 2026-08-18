58a96e8dc4888e13b1da0a650789ddf26e43eca4
parth5757 <psthakkar2@gmail.com>
2026-08-18 21:58:50 +0530

    Orders models created.

--- backend/orders/admin.py
+++ backend/orders/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Customer, SalesOrder, OrderItem
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("name", "phone", "gstin", "created_at")
+    search_fields = ("name", "phone", "gstin")
+
+
+class OrderItemInline(admin.TabularInline):
+    model  = OrderItem
+    extra  = 1
+
+@admin.register(SalesOrder)
+class SalesOrderAdmin(admin.ModelAdmin):
+    list_display = ("invoice number", "customer", "grand_total", "status", "order_date")
+    list_filter = ("status")
+    search_fields = ("invoice_number", "customer_name")
+    inlines = {OrderItemInline}
--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.conf import settings
+from inventory.models import Product
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15, blank=True)
+    email = models.EmailField(blank=True)
+    gstin = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+    state_code = models.CharField(max_length=2, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class SalesOrder(models.Model):
+    STATUS_CHOICES = [
+        ("draft", "Draft"),
+        ("confirmed", "Confirmed"),
+        ("cancelled", "Cancelled"),
+    ]
+    invoice_number = models.CharField(max_length=20, unique=True)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    order_data = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+    cgst = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    sgst = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    igst = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    grand_total = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    notes = models.TextField(blank=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.invoice_number:
+            last =  SalesOrder.objects.order_by("id").last()
+            next_id = (last.id + 1) if last else 1
+            self.invoice_number = f"INV-{next_id:04d}"
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.invoice_number
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(SalesOrder, on_delete=models.CASCADE, related_name="item")
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    gst_rate = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    cgst_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    sgst_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    igst_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    line_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f"{self.product.name} * {self.quantity}"
+    
